3240fb505029a350b70c0216833a26797ac918a1
johan larsson <johan.larsson@ub.gu.se>
2015-04-22 15:13:02 +0200

    btn added and bound to value for dropdown

--- app/controllers/home/step1.js
+++ app/controllers/home/step1.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	needs: ['application']
+	needs: ['application'],
+	isDisabled: function() {
+		if (this.get("controllers.application.selectedOrdertype")) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}.property('controllers.application.selectedOrdertype')
 });
