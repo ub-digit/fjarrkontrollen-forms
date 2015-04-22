3ba6363cc1435ff23c703a815b2af634797be3f6
johan larsson <johan.larsson@ub.gu.se>
2015-04-22 15:16:47 +0200

    added more dummydata

--- app/routes/application.js
+++ app/routes/application.js
@@ -6,9 +6,9 @@ export default Ember.Route.extend({
 		var orderTypes = [];
 		orderTypes.pushObject({id:1, identifier: 'loan', title: 'Lån'});
 		orderTypes.pushObject({id:2, identifier: 'article-copy', title: 'Artikelkopia'});
-	/*	orderTypes.pushObject({id:3, identifier: 'book-chapter-copy', title: 'Kopia av bokkapitel', validFor: ['forskare_anstalld','student','sahlgrenska', 'privatperson', 'foretag', 'ovriga', 'distansstudent']});
-		orderTypes.pushObject({id:4, identifier: 'scores', title: 'Musiktryck', validFor: ['forskare_anstalld','student', 'distansstudent']});
-		orderTypes.pushObject({id:5, identifier: 'micro-film', title: 'Lån mikrofilm', validFor: ['forskare_anstalld','student','sahlgrenska', 'privatperson', 'foretag', 'ovriga', 'distansstudent']});*/
+		orderTypes.pushObject({id:3, identifier: 'book-chapter-copy', title: 'Kopia av bokkapitel'});
+		orderTypes.pushObject({id:4, identifier: 'scores', title: 'Musiktryck'});
+		orderTypes.pushObject({id:5, identifier: 'micro-film', title: 'Lån mikrofilm'});
 		controller.set("orderTypes", orderTypes);
 		controller.set("selectedOrdertype", null);
 	}
