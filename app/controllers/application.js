f1adb0058a99fe699397574c79b1526e9f7890c2
Magnus Kull <magnus.kull@ub.gu.se>
2015-04-27 16:34:04 +0200

    added comment and not valid after date

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -8,8 +8,10 @@ export default Ember.Controller.extend({
 		issn: null,
 		publicationYear: null,
 		issue: null,
-		volume: null, 
+		volume: null,
 		pages: null,
+		notValidAfter: null,
+		comment: null
 	},
 
 	resetAllData: function() {
