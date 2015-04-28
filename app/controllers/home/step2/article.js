a8e973876dffc21b0098a528bfc2ebaebafd38c3
johan larsson <johan.larsson@ub.gu.se>
2015-04-28 13:20:44 +0200

    prev and next buttons step2/article

--- app/controllers/home/step2/article.js
+++ app/controllers/home/step2/article.js
@@ -2,6 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   needs: ['application'],
+  pubMedId: null,
 
-  pubMedId: null
+	isEnabled: Ember.computed.and('controllers.application.selectedOrdertype', 'controllers.application.selectedLocation'),
+	actions: {
+		back: function() {
+			this.transitionToRoute("home.step1");
+		}
+	}
+
+
 });
