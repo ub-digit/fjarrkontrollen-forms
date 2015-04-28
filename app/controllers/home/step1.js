74dcecfac2bb03b70fa456cad611ec6afec8df49
Magnus Kull <magnus.kull@ub.gu.se>
2015-04-28 10:14:59 +0200

    added checkboxes to book form

--- app/controllers/home/step1.js
+++ app/controllers/home/step1.js
@@ -2,12 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 	needs: ['application'],
-	isDisabled: function() {
-		if (this.get("controllers.application.selectedOrdertype")) {
-			return false;
-		}
-		else {
-			return true;
-		}
-	}.property('controllers.application.selectedOrdertype')
+
+	isDisabled: Ember.computed.and('controllers.application.selectedOrdertype', 'controllers.application.selectedLocation')
+	
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -16,8 +16,8 @@ export default Ember.Route.extend({
 		controller.set("selectedLocation", null);
 
 		var orderTypes = [];
-		orderTypes.pushObject({id:1, identifier: 'loan', title: 'Lån', title_en: 'Loan'});
-		orderTypes.pushObject({id:2, identifier: 'article-copy', title: 'Artikelkopia', title_en: 'Photocopies'});
+		orderTypes.pushObject({id:1, identifier: 'book', title: 'Lån', title_en: 'Loan'});
+		orderTypes.pushObject({id:2, identifier: 'article', title: 'Artikelkopia', title_en: 'Photocopies'});
 		orderTypes.pushObject({id:3, identifier: 'book-chapter-copy', title: 'Kopia av bokkapitel', title_en: 'Book chapter'});
 		orderTypes.pushObject({id:4, identifier: 'scores', title: 'Musiktryck', title_en: 'scores'});
 		orderTypes.pushObject({id:5, identifier: 'micro-film', title: 'Lån mikrofilm', title_en: 'Loan micro-film'});
