01b429188b69873709e9ddf0b49a846de4ddcc1b
johan larsson <johan.larsson@ub.gu.se>
2015-04-28 13:33:04 +0200

    next logic

--- app/controllers/home/step2/article.js
+++ app/controllers/home/step2/article.js
@@ -3,8 +3,15 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   needs: ['application'],
   pubMedId: null,
+  	isEnabled: function() {
+  		if (this.get('controllers.application.orderDetails.pages') && this.get('controllers.application.orderDetails.publicationYear')) {
+  			return true;
+  		}
+  		else {
+  			return false;
+  		}
+  	}.property('controllers.application.orderDetails.pages','controllers.application.orderDetails.publicationYear'),
 
-	isEnabled: Ember.computed.and('controllers.application.selectedOrdertype', 'controllers.application.selectedLocation'),
 	actions: {
 		back: function() {
 			this.transitionToRoute("home.step1");
