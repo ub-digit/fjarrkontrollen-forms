7a6ad1b8e2eb89b77d75d49f3eb8fd1ea10df092
Magnus Kull <magnus.kull@ub.gu.se>
2015-04-29 16:37:25 +0200

    added next and back to book template

--- app/controllers/home/step2/book.js
+++ app/controllers/home/step2/book.js
@@ -2,4 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   needs: ['application'],
+
+  isNextEnabled: function() {
+    if (this.get('controllers.application.orderDetails.book.bookTitle') && this.get('controllers.application.orderDetails.book.authors')) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }.property('controllers.application.orderDetails.book.bookTitle', 'controllers.application.orderDetails.book.authors'),
+
+  actions: {
+		back: function() {
+			this.transitionToRoute("home.step1");
+		},
+		nextStep: function() {
+			this.transitionToRoute("home.step3");
+		}
+  }
 });
