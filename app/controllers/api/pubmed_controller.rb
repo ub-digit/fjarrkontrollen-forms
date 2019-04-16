9f6cbe11148840432c4fc664495bf3eb54bdae25
Per Larsson <per.larsson@ub.gu.se>
2019-04-16 17:08:25 +0200

    Add missing protocoll in pubmed url

--- app/controllers/api/pubmed_controller.rb
+++ app/controllers/api/pubmed_controller.rb
@@ -3,7 +3,7 @@ class Api::PubmedController < ApplicationController
   def show
     begin
       id = params[:id]
-      response = RestClient.get "//eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&id=#{id}&retmode=json"
+      response = RestClient.get "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&id=#{id}&retmode=json"
       render body: response.body, status: 200, content_type: 'application/json'
     rescue RestClient::Exception => e
       render json: {error: e.message}, status: e.http_code
