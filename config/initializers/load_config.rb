2f89d71fe56afc4e95d80dd75f312b38b6c4bb14
David Gustafsson <glasklas@gmail.com>
2021-01-28 17:25:05 +0100

    Fix syntax error in load_config.rb

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,7 +1,7 @@
 require "erb"
 require "yaml"
 
-secret_config_file = Rails.env == 'test' ? "config/config_secret.test.yml" ? "config/config_secret.yml"
+secret_config_file = Rails.env == 'test' ? "config/config_secret.test.yml" : "config/config_secret.yml"
 yaml = Pathname.new("#{Rails.root}/#{secret_config_file}")
 secret_config = YAML.load(ERB.new(yaml.read).result(binding))
 APP_CONFIG = secret_config
