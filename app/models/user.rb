c8da95d53e10ae59ac2b7b022fc48558c55fa91d
David Gustafsson <david.gustafsson@ub.gu.se>
2025-02-25 15:42:17 +0100

    Fix Koha authentication GET param names

--- app/models/user.rb
+++ app/models/user.rb
@@ -67,8 +67,9 @@ class User
     user =  APP_CONFIG['koha']['user']
     password =  APP_CONFIG['koha']['password']
 
-    url = "#{base_url}/members/get?borrower=#{username}&userid=#{user}&password=#{password}"
+    url = "#{base_url}/members/get?borrower=#{username}&login_userid=#{user}&login_password=#{password}"
     response = RestClient.get url
+
     item = self.new username, response.body
     return item
   rescue => error
@@ -82,7 +83,7 @@ class User
     user =  APP_CONFIG['koha']['user']
     password =  APP_CONFIG['koha']['password']
 
-    url = "#{base_url}/members/auth?cardnumber=#{cardnumber}&personalnumber=#{personalnumber}&userid=#{user}&password=#{password}"
+    url = "#{base_url}/members/auth?cardnumber=#{cardnumber}&personalnumber=#{personalnumber}&login_userid=#{user}&login_password=#{password}"s
     response = RestClient.get url
     xml = Nokogiri::XML(response.body).remove_namespaces!
     if xml.search('//response/match').text.present?
