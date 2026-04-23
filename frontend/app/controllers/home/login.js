621455c1b8f95de9bc29859acad552aff19c3c5a
Per Larsson <per.larsson@ub.gu.se>
2026-04-23 09:10:20 +0200

    Remove handling of obsolete password active flag

--- frontend/app/controllers/home/login.js
+++ frontend/app/controllers/home/login.js
@@ -37,9 +37,6 @@ export default Controller.extend({
       this.transitionToRoute(step);
     }
   },
-  passwordActive: Ember.computed(function() {
-    return (ENV.passwordActive === 'true');
-  }),
   passwordForgotLink: Ember.computed('i18n.locale', function() {
     var lang = this.get('i18n.locale');
     return (lang === 'en') ? ENV.passwordForgotLinkEn : ENV.passwordForgotLinkSv;
